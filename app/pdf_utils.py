ee6124c61b7f83da9b53b56194d5bc62f557c951
geestg <glenstg15@gmail.com>
2026-03-06 10:42:52 +0700

    ulang

--- app/pdf_utils.py
+++
@@ -1,8 +0,0 @@
-from pypdf import PdfReader
-
-def extract_text_from_pdf(file_path: str):
-    reader = PdfReader(file_path)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text() + "\n"
-    return text
--- app/test_db.py
+++
@@ -1,9 +0,0 @@
-# test_db.py
-from app.database import engine
-
-try:
-    conn = engine.connect()
-    print("Koneksi database berhasil")
-    conn.close()
-except Exception as e:
-    print("Terjadi kesalahan:", e)
